1fbe62ecc89224cc72626187c03d752165b6fa72
othmanbrahman <othmanbinrahman@gmail.com>
2023-06-07 21:14:05 +0800

    added getOne updateOne and deleteOne

--- controllers/entry-controller.js
+++ controllers/entry-controller.js
@@ -1,6 +1,12 @@
 const {v4: uuidv4} = require("uuid");
 const httpStatus = require("http-status");
-const {create: createEntry, list: listEntry} = require("../services/entry.service"); // Add this line
+const {
+    create: createEntry,
+    list: listEntry, 
+    getOne: findById, 
+    updateOne: updateOneEntry, 
+    deleteOne: deleteOneEntry 
+} = require("../services/entry.service");
 const data = [];
 
 // Refactor the `create` function
@@ -26,41 +32,46 @@ const readAll = async (req, res) => {
     
 }
 
-const getOne = (req, res) => {
-    const found = data.find(d => d.id === req.params.id);
-    if(found){
-        res.json(found);
-    }else{
-        res.sendStatus(httpStatus.NOT_FOUND);
-    }
-}
-
-const updateOne = (req, res) => {
-    const id = req.params.id;
-    const dataToUpdate = req.body;
-
-    let found = data.find(d => d.id === id);
-    if(found){
-        for(const prop in dataToUpdate){
-            found[prop] = dataToUpdate[prop];
+const getOne = async (req, res) => {
+    try{
+        const entry = await findById(req.params.id);
+        if(entry){
+            return res.json(entry);
         }
-        return res.sendStatus(httpStatus.OK)
+        return res.sendStatus(httpStatus.NOT_FOUND);
+    }catch(e){
+        console.log(`Failed to find entry by id ${req.params.id}`, e);
+        return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
     }
-
-    return res.sendStatus(httpStatus.NOT_FOUND);
 }
 
-const deleteOne = (req, res) => {
-    const id = req.params.id;
-
-    const index = data.findIndex(d => d.id === id);
-    if(index > -1){
-        data.splice(index, 1);
-        return res.sendStatus(httpStatus.OK);
+const updateOne = async (req, res) => {
+    const condition = {
+        _id:req.params.id
     }
-    return res.sendStatus(httpStatus.NOT_FOUND);
-    
+    const data = req.body;
 
+    try{
+        const mRes = await updateOneEntry(condition, data);
+        if (mRes.matchedCount === 0){
+            return res.sendStatus(httpStatus.NOT_FOUND);
+        }
+        return res.json(mRes);
+        
+    }catch(e){
+        console.log(`Failed to update entry by id ${req.params.id}`, e);
+        return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
+const deleteOne = async (req, res) => {
+    try{
+        const status = await deleteOneEntry(req.params.id);
+        return status === 0 ? res.sendStatus(httpStatus.NOT_FOUND) : res.sendStatus(httpStatus.OK);
+    }catch(e){
+        console.log(`Failed to delete entry by id ${req.params.id}`, e);
+        return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
 
 module.exports = {
--- services/entry.service.js
+++ services/entry.service.js
@@ -16,8 +16,39 @@ const list = async () => {
         throw e;
     }
 }
+const getOne = async (id) => {
+    try{
+        return await Entry.findById(id);
+    }catch(e){
+        throw e;
+    }
+}
+
+const updateOne = async (condition, data) => {
+    try{
+        return await Entry.updateOne(condition, data);
+    }catch(e){
+        throw e;
+    }
+}
+
+const deleteOne = async (id) => {
+    try{
+        const entry = await Entry.findById(id);
+        if(!entry){
+            return 0;
+        }
+        await entry.deleteOne();
+        return 1;
+    }catch(e){
+        throw e;
+    }
+}
 
 module.exports = {
     create,
-    list
+    list,
+    getOne,
+    updateOne,
+    deleteOne
 }
